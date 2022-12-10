d1b45cb7ca0485b214b0be685fba608aa39b6292
nogayam <nogayamamity@gmail.com>
2022-12-10 23:30:55 +0200

    new temps

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ OUTPUT_PATH = r'/Users/nogayamamitai/Documents/LAB/lab-analysis-csvs/{name}.csv'
 OUTPUT_FILE_NAME = '28-11-23'
 ANALYSIS_MODULES = {"max_height": 1, "moving_average": 0,
                     "time_analysis": 0}
-TEMP = ['290', '292', '294', '295', '296', '298', '300', '302', '304', '305', '306', '308']
+TEMP = ['290', '292', '294', '295', '296', '298', '300', '302', '304', '305', '306', '308', '310']
 
 
